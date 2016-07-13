77844a8ba3d35d7f05cb31cf12be2e830f645f5c
root <k.on.trumpet@gmail.com>
2016-07-13 18:33:38 +0000

    Revert "pitch bend"
    This reverts commit 83ddb77aefcd0c07966595dd48900a8575f3ae5c.

--- index-kato2.js
+++ index-kato2.js
@@ -15,23 +15,20 @@ console.log('MRAA Version: ' + mraa.getVersion());
 
 /* Button */
 // Create the button object using GPIO pin 0
-/*var button0 = new upm_groveSensor.GroveButton(0);
+var button0 = new upm_groveSensor.GroveButton(0);
 var button1 = new upm_groveSensor.GroveButton(3);
 var button2 = new upm_groveSensor.GroveButton(5);
 var button3 = new upm_groveSensor.GroveButton(7);
 var groveRotary = new upm_groveSensor.GroveRotary(0);
-*/
 
 /* RotaryEncoder */
-//var rotaryEncoder = require("jsupm_rotaryencoder");
+var rotaryEncoder = require("jsupm_rotaryencoder");
 // Instantiate a Grove Rotary Encoder, using signal pins D2 and D3
-/*var myRotaryEncoder0 = new rotaryEncoder.RotaryEncoder(2,3);
+var myRotaryEncoder0 = new rotaryEncoder.RotaryEncoder(2,3);
 var myRotaryEncoder1 = new rotaryEncoder.RotaryEncoder(4,5);
 var myRotaryEncoder2 = new rotaryEncoder.RotaryEncoder(6,7);
 var myRotaryEncoder3 = new rotaryEncoder.RotaryEncoder(8,9);
-*/
 
-/* Joystick */
 var joystick = require('jsupm_joystick12');
 // Instantiate a joystick on analog pins A0 and A1
 var myJoystick = new joystick.Joystick12(2, 3);
@@ -41,49 +38,56 @@ var joystick_x = null;
 var LCD = require('jsupm_i2clcd');
 
 // Initialize Jhd1313m1 at 0x62 (RGB_ADDRESS) and 0x3E (LCD_ADDRESS)
-//var myLcd = new LCD.Jhd1313m1(0, 0x3E, 0x62);
+var myLcd = new LCD.Jhd1313m1(0, 0x3E, 0x62);
 
 setInterval(function(){
-//    myLcd.setCursor(0,0);
+    myLcd.setCursor(0,0);
     // RGB Blue
     //myLcd.setColor(53, 39, 249);
     // RGB Red
-/*    myLcd.setColor(255, 0, 0);
+    myLcd.setColor(255, 0, 0);
     myLcd.write('Hello World');
     myLcd.setCursor(1,2);
-*//*    console.log(button0.name() + "0 value is " + button0.value());
+    console.log(button0.name() + "0 value is " + button0.value());
     console.log(button1.name() + "1 value is " + button1.value());
     console.log(button2.name() + "2 value is " + button2.value());
     console.log(button3.name() + "3 value is " + button3.value());
-*/
+
     // Rotary
-//    var abs = groveRotary.abs_value();
+    var abs = groveRotary.abs_value();
+    var absdeg = groveRotary.abs_deg();
+    var absrad = groveRotary.abs_rad();
+
+    var rel = groveRotary.rel_value();
+    var reldeg = groveRotary.rel_deg();
+    var relrad = groveRotary.rel_rad();
 
     //write the knob value to the console in different formats
 
-//    console.log("Volume: " + abs);
+    console.log("Volume: " + abs);
     //console.log("Rel: " + rel + " " + Math.round(parseInt(reldeg)) + " " + relrad.toFixed(3));
 
     // joystick
     var x = roundNum(myJoystick.getXInput(), 6);
-    var xx = Math.max(-1, Math.min((x + 0.57) / 0.27 * 2 + 1, 1));
 
     if (joystick_x != x) {
         joystick_x = x;
-        synth.setPitchDiff(xx);
+        synth.setPitchDiff(Math.max(-1, Math.min(x / 0.17, 1)));
     }
 
     var XString = "Driving X:" + x;
     var YString = ": and Y:" + roundNum(myJoystick.getYInput(), 6);
-    //console.log(XString + YString);
-    console.log("x", xx, "y", myJoystick.getYInput());
+    console.log(XString + YString);
 
-//    myLcd.setCursor(0,0);
+    myLcd.setCursor(0,0);
     // RGB Blue
     //myLcd.setColor(53, 39, 249);
     // RGB Red
-//    myLcd.setColor(255, 0, 0);
-//    myLcd.write('WAVE: Saw   Square   Sign');
+    var p0 = myRotaryEncoder0.position();
+    var p1 = myRotaryEncoder1.position();
+    var p2 = myRotaryEncoder2.position();
+    myLcd.setColor(255, 0, 0);
+    myLcd.write('WAVE: Saw   Square   Sign');
 },300);
 
 // When exiting: clear interval and print message
